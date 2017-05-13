c4eb843fa4022b0e0207cee57c9b57ddad514296
Jagadeesh <ajagadeesh@gleedtech.com>
2017-05-13 19:06:14 +0530

    sale list code changes

---
+++ src/app/dashboards/dashboard.service.js
@@ -0,0 +1,13 @@
+  /**
+ * DASHBOARD PAGE SERVICE
+ */
+
+app.factory('dashboardService', ['$http', function($http) {
+    var dashboardServiceMethods = {};
+     var baseURL = "http://ec2-52-43-72-177.us-west-2.compute.amazonaws.com/";
+
+      
+
+         
+        return dashboardServiceMethods;
+    }])

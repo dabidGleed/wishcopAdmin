c1048b9b74d13bdce9c73482acffeed0546bcda0
Jagadeesh <ajagadeesh@gleedtech.com>
2017-07-18 18:44:32 +0530

    account transfer module code update

---
+++ src/app/account-transfer/account-transfer.service.js
@@ -0,0 +1,36 @@
+  /**
+   * USERS LIST PAGE SERVICE
+   */
+
+  app.factory('accountTransferService', ['$http', 'globalVars', function ($http, globalVars) {
+      var accountTransferServiceMethods = {};
+      var baseURL = globalVars.baseURL;
+      var userId = globalVars.userData.userId;
+
+      //service to get all users list
+      accountTransferServiceMethods.getAccountTransferList = function () {
+
+          var finalUrl = baseURL + "accountdetails/" + userId + "/list";
+          return $http({
+              method: 'GET',
+              url: finalUrl
+              // data: dataObj,
+              // headers:  
+          })
+      };
+
+      //service to update status
+      accountTransferServiceMethods.updateStatus = function (id) {
+          var finalUrl = baseURL + "accountdetails/"+id+"/update/transferred"
+          return $http({
+              method: 'PUT',
+              url: finalUrl
+              // data: dataObj,
+              // headers:  
+          })
+      };
+ 
+
+
+      return accountTransferServiceMethods;
+  }])

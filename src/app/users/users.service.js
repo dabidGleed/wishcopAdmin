78dd290ceed12c1345e1e6bab52d85c33814d327
Sriram277 <sriramakiran727@gmail.com>
2018-05-19 15:55:59 +0530

    issues fixed and competetion code commented

--- src/app/users/users.service.js
+++ src/app/users/users.service.js
@@ -1,67 +1,59 @@
   /**
- * USERS LIST PAGE SERVICE
- */
-   
-app.factory('usersService', ['$http','globalVars', function($http,globalVars) {
-    var usersServiceMethods = {};
-     var baseURL = globalVars.baseURL;
+   * USERS LIST PAGE SERVICE
+   */
 
-         //service to get all users list
-        usersServiceMethods.getUsersList = function() {
-             
-            var finalUrl = baseURL + "admin/users/get/all"
-            return $http({
-                method: 'POST',
-                url: finalUrl
-                // data: dataObj,
-                // headers:  
-            }); 
-        };
-        usersServiceMethods.getCompetitorsList = function() {
-            
-           var finalUrl = baseURL + "competitors/list/all"
-           return $http({
-               method: 'GET',
-               url: finalUrl
-           }) 
-        };
-         //service to active user
-        usersServiceMethods.activeUserStatus = function(userId) {
-             
-            var finalUrl = baseURL + "admin/"+userId+"/make/active"
-            return $http({
-                method: 'POST',
-                url: finalUrl
-                // data: dataObj,
-                // headers:  
-            }) 
-        };
+  app.factory('usersService', ['$http', 'globalVars', function ($http, globalVars) {
+      var usersServiceMethods = {};
+      var baseURL = globalVars.baseURL;
 
-        //service to get user profile Detials
-        usersServiceMethods.userprofileDetials = function(userId) {
-             
-            var finalUrl = baseURL + "admin/"+userId+"/userprofile";
-            return $http({
-                method: 'GET',
-                url: finalUrl
-                // data: dataObj,
-                // headers:  
-            }) 
-        };
+      //service to get all users list
+      usersServiceMethods.getUsersList = function () {
+
+          var finalUrl = baseURL + "admin/users/get/all";
+          return $http({
+              method: 'POST',
+              url: finalUrl
+          });
+      };
+      usersServiceMethods.getCompetitorsList = function () {
+
+          var finalUrl = baseURL + "competitors/list/all";
+          return $http({
+              method: 'GET',
+              url: finalUrl
+          });
+      };
+      //service to active user
+      usersServiceMethods.activeUserStatus = function (userId) {
+
+          var finalUrl = baseURL + "admin/" + userId + "/make/active";
+          return $http({
+              method: 'POST',
+              url: finalUrl
+          });
+      };
+
+      //service to get user profile Detials
+      usersServiceMethods.userprofileDetials = function (userId) {
+
+          var finalUrl = baseURL + "admin/" + userId + "/userprofile";
+          return $http({
+              method: 'GET',
+              url: finalUrl
+          });
+      };
 
 
-         //service to delete user
-        usersServiceMethods.deleteUserStatus = function(userId) {
-             
-            var finalUrl = baseURL + "admin/"+userId+"/delete"
-            return $http({
-                method: 'DELETE',
-                url: finalUrl
-                // data: dataObj,
-                // headers:  
-            }) 
-        };
+      //service to delete user
+      usersServiceMethods.deleteUserStatus = function (userId) {
 
-         
-        return usersServiceMethods;
-    }])
+          var finalUrl = baseURL + "admin/" + userId + "/delete";
+          return $http({
+              method: 'DELETE',
+              url: finalUrl
+          });
+      };
+
+
+      return usersServiceMethods;
+  }]);
